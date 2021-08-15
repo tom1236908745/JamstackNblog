812c7a478b39d2878959fb39394da18f7ba176f0
tom1236908745 <tom1233908745@gmail.com>
2021-08-15 20:29:37 +0900

    add outフォルダ

--- out/_next/static/h561AI4GKZYLDsWDEweUJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-84aa0e126efeda99be5c.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/blogs/[id]":["static/chunks/pages/blogs/[id]-f6d80efc3de57b380ae2.js"],sortedPages:["/","/_app","/_error","/blogs/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
